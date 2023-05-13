9bedd5643774775aae1a4497611dbda1cefc6de1
Vassil Tchifligarov <tchifligarov@gmail.com>
2023-05-13 11:17:58 +0200

    Add tabulate modul

--- bikeshare_submitted.py
+++ bikeshare_submitted.py
@@ -1,7 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
-
+import tabulate as tabulate
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
              'washington': 'washington.csv'}
